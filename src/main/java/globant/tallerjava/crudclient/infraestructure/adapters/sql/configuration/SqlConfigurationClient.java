7d7fd8405f761503f92161d16180ad1a201d3ad0
LinaDev14 <perfilgitlinamaria@gmail.com>
2023-07-17 23:02:43 -0500

    se implementan metodos en sqlConfigurationClient y sqlConfigurationRoutine

--- src/main/java/globant/tallerjava/crudclient/infraestructure/adapters/sql/configuration/SqlConfigurationClient.java
+++ src/main/java/globant/tallerjava/crudclient/infraestructure/adapters/sql/configuration/SqlConfigurationClient.java
@@ -1,9 +1,19 @@
 package globant.tallerjava.crudclient.infraestructure.adapters.sql.configuration;
 
+import globant.tallerjava.crudclient.domain.entities.Client;
 import globant.tallerjava.crudclient.infraestructure.adapters.dto.ClientDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SqlConfigurationClient extends JpaRepository<ClientDto, Long> {
+    List<ClientDto> findByName (String name);
+    List<ClientDto> findByAge (String age);
+    List<ClientDto> findByHeight(String height);
+    ClientDto saveClient(ClientDto clientDto);
+    ClientDto updateClient(ClientDto clientDto);
+    List<ClientDto> getAllClients();
+    ClientDto deleteClient(int id);
 }
--- src/main/java/globant/tallerjava/crudclient/infraestructure/adapters/sql/configuration/SqlConfigurationRoutine.java
+++ src/main/java/globant/tallerjava/crudclient/infraestructure/adapters/sql/configuration/SqlConfigurationRoutine.java
@@ -1,9 +1,20 @@
 package globant.tallerjava.crudclient.infraestructure.adapters.sql.configuration;
 
+import globant.tallerjava.crudclient.domain.entities.Routine;
 import globant.tallerjava.crudclient.infraestructure.adapters.dto.RoutineDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SqlConfigurationRoutine extends JpaRepository<RoutineDto, Long> {
+
+    List<RoutineDto> findByName (String name);
+    List<RoutineDto> findByDuration(String duration);
+    List<RoutineDto> findByDifficultyLevel(String difficultyLevel);
+    RoutineDto saveRoutine(RoutineDto routineDto);
+    RoutineDto updateRoutine(RoutineDto routineDto);
+    List<RoutineDto> getAllRoutines();
+    RoutineDto deleteRoutine(int id);
 }
